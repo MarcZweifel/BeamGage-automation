56bc8e17f424b60394b4cabdee57373c539ca80e
MarcZweifel <marc.zweifel@gmail.com>
2023-05-16 12:55:49 +0200

    Initialization

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Spiricon.Automation;
+using System.Linq;
+
+namespace BGCSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runMe = new Runner();
+            runMe.Run();
+            
+        }
+    }
+
+    public class Runner
+    {
+        // Declare the BeamGage Automation client
+        private AutomatedBeamGage _bg;
+
+        public void Run()
+        {
+            Console.WriteLine("Press enter to exit.\n");
+            // Start BeamGage Automation client
+            _bg = new AutomatedBeamGage("ClientOne", false);
+
+            
+
+            // Create and register for the new frame event
+            //new AutomationFrameEvents(_bg.ResultsPriorityFrame).OnNewFrame += NewFrameFunction;
+            
+            
+            
+            Console.Read();
+
+            // Shutdown BeamGage
+            _bg.Instance.Shutdown();
+        }
+    }
+}
